f8da115b72f95ff10ecdc1b6b543e6eec3219490
peter <petecruckshank@gmail.com>
2018-09-25 20:30:32 -0400

    added access to the data & append to list

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ const ItemCtrl = (function() {
 
   // Public methods
   return {
+    getItems: function() {
+      return data.items;
+    },
     logData: function() {
       return data;
     }
@@ -30,8 +33,26 @@ const ItemCtrl = (function() {
 
 // UI Controller
 const UICtrl = (function() {
+  const UISelectors = {
+    itemList: '#item-list'
+  }
   // Public methods
-  return {};
+  return {
+    populateItemList: function(items)  {
+      let html = '';
+
+      items.forEach(function(item) {
+        html += `<li class="collection-item" id="item-${item.id}">
+        <strong>${item.name} </strong> <em>${item.calories} Calories</em>
+        <a href="#" class="secondary-content">
+          <i class="edit-item fa fa-pencil"></i>
+        </a>
+      </li>`;
+        
+      });
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    }
+  }
 })();
 
 // App Controller
@@ -39,7 +60,12 @@ const App = (function(ItemCtrl, UICtrl) {
   // Public methods
   return {
     init: function() {
-      console.log("Initializing App...");
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+
+      // Populate list with items
+      UICtrl.populateItemList(items);
     }
   };
 })(ItemCtrl, UICtrl);
